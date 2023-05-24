e9c5b059ca7fa56287e7d1c7c923d2a904d45e2e
irfandi <irfandi.nutech@gmail.com>
2023-05-24 10:36:54 +0700

    [Update] - Fix minor

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -6,5 +6,5 @@ const getFiles = (filenames) => {
 
 module.exports = {
 	"**/*.{ts,tsx}": [getFiles],
-	"**/*.{js,ts,tsx,json,md}": "prettier --write",
+	"**/*.{js,cjs,ts,tsx,json,md}": "prettier --write",
 };
